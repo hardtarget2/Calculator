bad406d429094a27434b2b2c74168b7ff0f51624
Harry Bailey <bharry29@yahoo.com>
2020-03-11 23:13:38 -0400

    Completed the code

---
+++ calculator.js
@@ -0,0 +1,10 @@
+
+
+var x = prompt("Please Enter The First Number.", "0");
+var y = prompt("Please Enter The Second Number.", "0");
+
+
+var Num1 = parseInt(x);
+var Num2 = parseInt(y);
+
+alert("The Answer is: ", Number(Num1) + Number(Num2));
